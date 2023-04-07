46a5fd62050c47992a1dfb69133d99bb3cdd2569
Ben Erwin <github@powersimple.com>
2023-04-07 09:44:46 -0400

    Updates to foundation
    Not wpml added to functions
    Added SQL DB folder,
    Made .gitignore ignore webpack (for now) and package.lock.json

--- header.php
+++ header.php
@@ -3,8 +3,24 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+  <?php 
+  wp_head(); ?>
+<script>
+  <?php
+      // post specific hacks
+          if(function_exists('icl_object_id')){
+              global $sitepress;
+             print "let languages = ".json_encode(getLanguageList());
+          // $thumbnail =getThumbnail(get_post_thumbnail_id($post->ID),"Full");
+          }
+       
+  ?>
+  console.log(appz3)
+
+</script>
+
 </head>
 <body <?php body_class(); ?>>
 
 <!-- application  -->
+
