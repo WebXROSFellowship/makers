262b980059c4b0b0725f9c87a8a5664953db0287
Aryan Grover <aryangrover230402@gmail.com>
2023-04-17 19:23:28 +0530

    Integrating API

--- src/js/app/Components/Navbar.js
+++ src/js/app/Components/Navbar.js
@@ -1,107 +1,134 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./../../../scss/style.scss";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [navbarMenus, setNavbarMenus] = useState();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    let stagingData = await fetch(
+      "https://staging.webxr.link/wp-json/wp/v2/menus?menus"
+    );
+    let jsonData = await stagingData.json();
+    setNavbarMenus(jsonData);
+    let items = jsonData[0].items;
+    let head = items[0];
+    items = items.slice(1);
+    let data = [
+      {
+        name: head,
+        items: items,
+      },
+    ];
+    setNavbarMenus(data);
+  }
 
   return (
     <>
       <nav className="navbar">
-        <div className="navbar-brand">The Polys WebXR Awards and Summit Series</div>
+        <div className="navbar-brand">
+          The Polys WebXR Awards and Summit Series
+        </div>
         <div className="navbar-right">
-          <div className="dropdown">
-            <button className="dropbtn">WEBXR SUMMIT SERIES</button>
-            <div className="dropdown__content">
-              <span
-                href="https://www.google.com"
-                target="_blank"
-                className="dropdown__items"
-              >
-                WEBXR PRODUCTION SUMMIT
-              </span>
-              <span href="#" className="dropdown__items">
-                WEBXR BRAND SUMMIT
-              </span>
-              <span className="dropdown__items">WEBXR EDUCATION SUMMIT</span>
-              <span className="dropdown__items">WEBXR DESIGN SUMMIT</span>
-              <span className="dropdown__items">WEBXR BUSINESS SUMMIT</span>
-              <span className="dropdown__items">WEBXR DEVELOPER SUMMIT</span>
-            </div>
-          </div>
-          <div className="dropdown">
-            <button className="dropbtn">THE POLYS - WEBXR AWARDS</button>
-            <div className="dropdown__content">
-              <span className="dropdown__items">
-                THE 2ND POLYS - WEBXR AWARDS
-              </span>
-              <span className="dropdown__items">2021 WINNERS</span>
-              <span className="dropdown__items">
-                THE POLYS - 2020 VIRTUAL RED CARPET
-              </span>
-              <span className="dropdown__items">
-                THE POLYS - 2020 WEBXR AWARDS CEREMONY
-              </span>
-              <span className="dropdown__items">2020 AWARDS</span>
-            </div>
-          </div>
+          {navbarMenus ? (
+            navbarMenus.map((currEle, i) => (
+              <div className="dropdown" key={i}>
+                <button className="dropbtn">{currEle.name.title}</button>
+                <div className="dropdown__content">
+                  {currEle.items.map((item, i) => (
+                    <span className="dropdown__items" key={i}>
+                      {item.title}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            ))
+          ) : (
+            <>
+            </>
+          )}
           <div className="dropdown dropdown--logos dropdown__socials">
-            <a href="https://twitter.com/webxrawards" rel="noreferrer" target="_blank" >
-            <span className="dropdown__logo-img">
-            <i className="fa-brands fa-twitter fa-xl"></i>
-            </span>
+            <a
+              href="https://twitter.com/webxrawards"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <span className="dropdown__logo-img">
+                <i className="fa-brands fa-twitter fa-xl"></i>
+              </span>
             </a>
-            <a href="https://www.instagram.com/webxrawards/" rel="noreferrer" target="_blank">
-            <span className="dropdown__logo-img">
-            <i className="fa-brands fa-instagram fa-xl"></i>
-            </span>
+            <a
+              href="https://www.instagram.com/webxrawards/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <span className="dropdown__logo-img">
+                <i className="fa-brands fa-instagram fa-xl"></i>
+              </span>
             </a>
-            <a href="https://www.facebook.com/groups/webxrawards" rel="noreferrer" target="_blank">
-            <span className="dropdown__logo-img">
-            <i className="fa-brands fa-facebook fa-xl"></i>
-            </span>
+            <a
+              href="https://www.facebook.com/groups/webxrawards"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <span className="dropdown__logo-img">
+                <i className="fa-brands fa-facebook fa-xl"></i>
+              </span>
             </a>
-            <a href="https://www.linkedin.com/company/the-polys/" rel="noreferrer" target="_blank">
-            <span className="dropdown__logo-img">
-            <i className="fa-brands fa-linkedin fa-xl"></i>
-            </span>
+            <a
+              href="https://www.linkedin.com/company/the-polys/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <span className="dropdown__logo-img">
+                <i className="fa-brands fa-linkedin fa-xl"></i>
+              </span>
             </a>
-            <a href="https://discord.gg/T5vRuM5cDS" rel="noreferrer" target="_blank">
-            <span className="dropdown__logo-img">
-            <i className="fa-brands fa-discord fa-xl"></i>
-            </span>
+            <a
+              href="https://discord.gg/T5vRuM5cDS"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <span className="dropdown__logo-img">
+                <i className="fa-brands fa-discord fa-xl"></i>
+              </span>
             </a>
           </div>
 
           <div className="hamburger">
-            <span className="hamburger__logo" onClick={()=>{
+            <span
+              className="hamburger__logo"
+              onClick={() => {
                 setShowMenu(!showMenu);
-            }}>
-            <i className="fa-solid fa-bars fa-xl"></i>
+              }}
+            >
+              <i className="fa-solid fa-bars fa-xl"></i>
             </span>
           </div>
 
           {showMenu === true ? (
             <div className="sideMenu">
-              <div className="dropdown2">
-                <button className="dropbtn">WEBXR SUMMIT SERIES</button>
-                <div className="dropdown__content">
-                  <span className="dropdown__items">WEBXR PRODUCTION SUMMIT</span>
-                  <span className="dropdown__items">WEBXR BRAND SUMMIT</span>
-                  <span className="dropdown__items">WEBXR EDUCATION SUMMIT</span>
-                  <span className="dropdown__items">WEBXR DESIGN SUMMIT</span>
-                  <span className="dropdown__items">WEBXR BUSINESS SUMMIT</span>
-                  <span className="dropdown__items">WEBXR DEVELOPER SUMMIT</span>
-                </div>
-              </div>
-              <div className="dropdown2">
-                <button className="dropbtn">THE POLYS - WEBXR AWARDS</button>
-                <div className="dropdown__content">
-                  <span className="dropdown__items">THE 2ND POLYS - WEBXR AWARDS</span>
-                  <span className="dropdown__items">THE POLYS - 2020 VIRTUAL RED CARPET</span>
-                  <span className="dropdown__items">THE POLYS - 2020 WEBXR AWARDS CEREMONY</span>
-                  <span className="dropdown__items">2020 AWARDS</span>
-                </div>
-              </div>
+              {navbarMenus ? (
+                navbarMenus.map((currEle, i) => (
+                  <div className="dropdown2" key={i}>
+                    <button className="dropbtn">{currEle.name.title}</button>
+                    <div className="dropdown__content">
+                      {currEle.items.map((item, i) => (
+                        <span className="dropdown__items" key={i}>
+                          {item.title}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <>
+                </>
+              )}
             </div>
           ) : (
             <></>
@@ -111,6 +138,5 @@ const Navbar = () => {
     </>
   );
 };
-
 
 export default Navbar;
