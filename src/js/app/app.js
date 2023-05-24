8610e22e1e63476d41563fe3601656ea278d4467
surbhixyyz <surbhi112122@gmail.com>
2023-05-24 22:26:16 +0530

    Main branch code merge

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -24,7 +24,7 @@ const appRouter = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <AFrame />,
+        element: <Body />,
       },
       {
         path: "aframe",
--- src_js_app_Components_AFrame_js.js
+++ src_js_app_Components_AFrame_js.js
@@ -1,39 +1,21 @@
 "use strict";
-(self["webpackChunkmakers"] = self["webpackChunkmakers"] || []).push([[832],{
+(self["webpackChunkmakers"] = self["webpackChunkmakers"] || []).push([["src_js_app_Components_AFrame_js"],{
 
-/***/ 832:
+/***/ "./src/js/app/Components/AFrame.js":
+/*!*****************************************!*\
+  !*** ./src/js/app/Components/AFrame.js ***!
+  \*****************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-// ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
-
-// EXPORTS
-__webpack_require__.d(__webpack_exports__, {
-  "default": () => (/* binding */ Components_AFrame)
-});
-
-;// CONCATENATED MODULE: ./node_modules/@babel/runtime/helpers/esm/extends.js
-function _extends() {
-  _extends = Object.assign ? Object.assign.bind() : function (target) {
-    for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i];
-      for (var key in source) {
-        if (Object.prototype.hasOwnProperty.call(source, key)) {
-          target[key] = source[key];
-        }
-      }
-    }
-    return target;
-  };
-  return _extends.apply(this, arguments);
-}
-// EXTERNAL MODULE: ./node_modules/react/index.js
-var react = __webpack_require__(294);
-;// CONCATENATED MODULE: ./src/js/app/psudo_data/assets.json
-const assets_namespaceObject = JSON.parse('[{"id":"marvel","type":"model","name":"astra","url":"https://cdn.glitch.global/b32f8a0e-a5aa-4181-890e-189ebc2588f0/marvel.glb"},{"id":"powersimple","type":"model","name":"powersimple","url":"https://cdn.glitch.global/b32f8a0e-a5aa-4181-890e-189ebc2588f0/powersimple.glb"},{"id":"astra","type":"model","name":"marvel","url":"https://cdn.glitch.com/ac5eecac-40b2-4897-8f67-28c497a19b47%2FAstronaut.glb"},{"id":"bg","type":"image","name":"background","url":"https://cdn.glitch.global/b32f8a0e-a5aa-4181-890e-189ebc2588f0/bg.jpg"}]');
-;// CONCATENATED MODULE: ./src/js/app/Components/dynamicContent.json
-const dynamicContent_namespaceObject = JSON.parse('[{"id":"#astra","gltf-model":"https://cdn.glitch.com/ac5eecac-40b2-4897-8f67-28c497a19b47%2FAstronaut.glb","position":"-1 1.93968 -3","crossorigin":"anonymous"},{"troika-text":"strokeColor: #fffafa; value: Text is here","id":"#text","position":"1.27063 1.34902 1","visible":"","rotation":"1 0 0"},{"id":"#marvel","gltf-model":"https://cdn.glitch.global/b32f8a0e-a5aa-4181-890e-189ebc2588f0/marvel.glb","position":"-3.60503 0.75 -3","scale":"3 3 3","crossorigin":"anonymous"}]');
-;// CONCATENATED MODULE: ./src/js/app/Components/AFrame.js
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _babel_runtime_helpers_extends__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/extends */ "./node_modules/@babel/runtime/helpers/esm/extends.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _psudo_data_assets_json__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./../psudo_data/assets.json */ "./src/js/app/psudo_data/assets.json");
+/* harmony import */ var _dynamicContent_json__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./dynamicContent.json */ "./src/js/app/Components/dynamicContent.json");
 
 
 
@@ -41,7 +23,7 @@ const dynamicContent_namespaceObject = JSON.parse('[{"id":"#astra","gltf-model":
 // have used native file system till endpoints unavailable
 
 function AFrame() {
-  const [loading, setLoading] = (0,react.useState)(true);
+  const [loading, setLoading] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(true);
   const color = new URLSearchParams(document.location.search).get("color");
   // Default Black color for the plane
   var gcolor = "#000000";
@@ -53,7 +35,7 @@ function AFrame() {
       gcolor = "#" + color;
     }
   }
-  (0,react.useEffect)(() => {
+  (0,react__WEBPACK_IMPORTED_MODULE_1__.useEffect)(() => {
     // loading inspector
     function loadAndGet() {
       var sceneEl = document.querySelector("a-scene");
@@ -122,7 +104,7 @@ function AFrame() {
     function updateDataFile(jsonString) {
       const newData = JSON.parse(jsonString);
       var foundData = false;
-      const updatedData = dynamicContent_namespaceObject.map(item => {
+      const updatedData = _dynamicContent_json__WEBPACK_IMPORTED_MODULE_3__.map(item => {
         if (item.id === newData.id) {
           console.log("Found the item to update");
           foundData = true;
@@ -185,58 +167,105 @@ function AFrame() {
     loadAndGet();
     addMani();
   }, []);
-  return /*#__PURE__*/react.createElement(react.Fragment, null, /*#__PURE__*/react.createElement("a-scene", null, /*#__PURE__*/react.createElement("a-camera", {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement((react__WEBPACK_IMPORTED_MODULE_1___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("a-scene", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("a-camera", {
     position: "0 1.2 0",
     rotation: "0 -45 0"
-  }, /*#__PURE__*/react.createElement("a-cursor", {
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("a-cursor", {
     id: "cursor",
     color: "#FF0000"
-  })), /*#__PURE__*/react.createElement("a-assets", null, assets_namespaceObject.map(asset => {
+  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("a-assets", null, _psudo_data_assets_json__WEBPACK_IMPORTED_MODULE_2__.map(asset => {
     if (asset.type === "model") {
-      return /*#__PURE__*/react.createElement("a-asset-item", {
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("a-asset-item", {
         id: asset.id,
         src: asset.url,
         key: asset.id,
         crossOrigin: "anonymous"
       });
     }
-    return /*#__PURE__*/react.createElement("img", {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("img", {
       id: asset.id,
       src: asset.url,
       key: asset.id,
       crossOrigin: "anonymous"
     });
-  })), loading ? /*#__PURE__*/react.createElement("p", null, "Loading...") : /*#__PURE__*/react.createElement(react.Fragment, null, /*#__PURE__*/react.createElement("a-entity", {
+  })), loading ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("p", null, "Loading...") : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement((react__WEBPACK_IMPORTED_MODULE_1___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("a-entity", {
     id: "#powersimple",
     "gltf-model": "#powersimple",
     position: "0 0.75 -3",
     radius: "0.5",
     height: "1.5",
     crossOrigin: "anonymous"
-  }), /*#__PURE__*/react.createElement("a-entity", {
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("a-entity", {
     id: "#marvel",
     "gltf-model": "#marvel",
     position: "1 0.75 -3",
     scale: "3 3 3",
     crossOrigin: "anonymous"
-  }), dynamicContent_namespaceObject.map(entity => /*#__PURE__*/react.createElement("a-entity", _extends({
+  }), _dynamicContent_json__WEBPACK_IMPORTED_MODULE_3__.map(entity => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("a-entity", (0,_babel_runtime_helpers_extends__WEBPACK_IMPORTED_MODULE_0__["default"])({
     key: entity.id
-  }, entity)))), /*#__PURE__*/react.createElement("a-sphere", {
+  }, entity)))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("a-sphere", {
     position: "0 0.7 -7",
     radius: "2.25",
     "change-color-on-hover": "color:#FFFFFF"
-  }), /*#__PURE__*/react.createElement("a-plane", {
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("a-plane", {
     rotation: "-90 0 0",
     position: "0 -2 -10",
     width: "10",
     height: "10",
     color: gcolor,
     shadow: true
-  }), /*#__PURE__*/react.createElement("a-sky", {
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1___default().createElement("a-sky", {
     src: "#bg"
   })));
 }
-/* harmony default export */ const Components_AFrame = (AFrame);
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AFrame);
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/extends.js":
+/*!************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/extends.js ***!
+  \************************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ _extends)
+/* harmony export */ });
+function _extends() {
+  _extends = Object.assign ? Object.assign.bind() : function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+    return target;
+  };
+  return _extends.apply(this, arguments);
+}
+
+/***/ }),
+
+/***/ "./src/js/app/Components/dynamicContent.json":
+/*!***************************************************!*\
+  !*** ./src/js/app/Components/dynamicContent.json ***!
+  \***************************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('[{"id":"#astra","gltf-model":"https://cdn.glitch.com/ac5eecac-40b2-4897-8f67-28c497a19b47%2FAstronaut.glb","position":"-1 1.93968 -3","crossorigin":"anonymous"},{"troika-text":"strokeColor: #fffafa; value: Text is here","id":"#text","position":"1.27063 1.34902 1","visible":"","rotation":"1 0 0"},{"id":"#marvel","gltf-model":"https://cdn.glitch.global/b32f8a0e-a5aa-4181-890e-189ebc2588f0/marvel.glb","position":"-3.60503 0.75 -3","scale":"3 3 3","crossorigin":"anonymous"}]');
+
+/***/ }),
+
+/***/ "./src/js/app/psudo_data/assets.json":
+/*!*******************************************!*\
+  !*** ./src/js/app/psudo_data/assets.json ***!
+  \*******************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('[{"id":"marvel","type":"model","name":"astra","url":"https://cdn.glitch.global/b32f8a0e-a5aa-4181-890e-189ebc2588f0/marvel.glb"},{"id":"powersimple","type":"model","name":"powersimple","url":"https://cdn.glitch.global/b32f8a0e-a5aa-4181-890e-189ebc2588f0/powersimple.glb"},{"id":"astra","type":"model","name":"marvel","url":"https://cdn.glitch.com/ac5eecac-40b2-4897-8f67-28c497a19b47%2FAstronaut.glb"},{"id":"bg","type":"image","name":"background","url":"https://cdn.glitch.global/b32f8a0e-a5aa-4181-890e-189ebc2588f0/bg.jpg"}]');
 
 /***/ })
 
