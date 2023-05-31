b1096afc8d8ce6da30177d92819d1365d2a9d8db
Ananya Jain <54437908+ananya2711@users.noreply.github.com>
2023-05-31 15:48:15 +0530

    Revert "Revert "Merge branch 'main' into ananya""
    This reverts commit 4ffbfe6087197b4481bc6395b0b3ac17257cd5b0.

--- src/js/app/Components/NavSites.js
+++ src/js/app/Components/NavSites.js
@@ -1,18 +1,72 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState, useMemo } from "react";
 import { useParams } from "react-router-dom";
 import DataContext from "../Utils/DataContext";
+import MenuDataContext from "../Utils/MenuDataContext";
 
 const NavSites = () => {
-  const { sitename } = useParams();
-  const { data } = useContext(DataContext);
-  const cd = data.filter((e) => e.slug === sitename);
-  const content = cd[0].content || "";
-  const userName = cd[0].title;
+  const { sitename, sn } = useParams();
+  const { lang } = useContext(DataContext);
+
+  const {menuData, setMenuData} = useContext(MenuDataContext);
+
+  // const [menuData, setMenuData] = useState([]);
+
+  // useEffect(() => {
+  //   fetchMenuData();
+  // }, [lang]);
+
+  // async function fetchMenuData() {
+  //   try {
+  //     let fetchURL2 = `https://staging.webxr.link/${lang}/wp-json/wp/v2/menus?menus`;
+  //     let stagingData = await fetch(fetchURL2);
+  //     let jsonData = await stagingData.json();
+  //     let items = jsonData.filter((item) => item.slug == "main-menu");
+  //     items = items[0].items;
+  //     setMenuData(items);
+  //   } catch (err) {
+  //     console.log("Error fetching menu data:" + err);
+  //   }
+  // }
+
+  // const filteredMenuData = useMemo(() => {
+  //   const curl = "/" + sitename + "/" + sn + "/";
+  //   const filteredData = menuData?.filter((item) => item.url == curl);
+  //   return filteredData.length > 0 ? filteredData[0].content : null;
+  // }, [menuData, sitename, sn]);
+
+  // const [menuData, setMenuData] = useState({});
+
+  useEffect(()=>{
+    fetchMenuData(lang);
+  }, [lang]);
+
+  async function fetchMenuData(lang) {
+    try {
+      let fetchURL = `https://staging.webxr.link/${lang}/wp-json/wp/v2/menus?menus`;
+      let stagingData = await fetch(fetchURL);
+      let jsonData = await stagingData.json();
+      let items = jsonData.filter((item) => item.slug === "main-menu");
+      items = items[0]?.items || [];
+      setMenuData(prevData => ({
+        ...prevData,
+        [lang]: items
+      }));
+    } catch (err) {
+      console.log("Error fetching menu data: " + err);
+    }
+  }
+  
+  const filteredMenuData = useMemo(() => {
+    const curl = "/" + sitename + "/" + sn + "/";
+    const langMenuData = menuData[lang] || [];
+    const filteredData = langMenuData.filter((item) => item.url === curl);
+    return filteredData.length > 0 ? filteredData[0].content : null;
+  }, [menuData, lang, sitename, sn]);
+  
 
   return (
     <>
-      <h1>{userName}</h1>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+      <div dangerouslySetInnerHTML={{ __html: filteredMenuData }} />
     </>
   );
 };
