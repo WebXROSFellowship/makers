db1e60a4fa7b69186dcddf3981eb13c3c3e00ad8
SHRAVANI KAWARE <Shravanikaware12@gmail.com>
2023-06-04 23:23:05 +0530

    image fetching done

--- src/js/app/Components/Profile.js
+++ src/js/app/Components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import DataContext from "../Utils/DataContext";
 
@@ -9,10 +9,26 @@ const Profile = () => {
   const content = cd[0].content || "";
   const userName = cd[0].title;
 
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://staging.webxr.link/wp-json/wp/v2/media?media`)
+      .then((response) => response.json())
+      .then((data) => {
+        const profileImage = data.find((image) => image.slug === username);
+        const imageUrl = profileImage ? profileImage.guid.rendered : "";
+        setImages([imageUrl]);
+      })
+      .catch((error) => console.log(error));
+  }, [username]);
+
   return (
     <>
       <h1>{userName}</h1>
       <div dangerouslySetInnerHTML={{ __html: content }} />
+      {images.map((imageUrl, index) => (
+        <img key={index} src={imageUrl} alt={`Image ${index}`} />
+      ))}
     </>
   );
 };
