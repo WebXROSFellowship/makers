06d6e2e985654e771533499e14524eb1fd97eea1
KUNAAL MANGAL <ksmangal1998@gmail.com>
2023-06-08 03:06:02 +0530

    update inspector rest_api created

--- config.php
+++ config.php
@@ -1,7 +1,26 @@
 <?php
-  define( 'SITE_URL', get_site_url() );
-  define( 'HOME_URL', get_home_url() );
-  define( 'SITE_TITLE', get_bloginfo( 'name' ) );
-  define( 'SITE_TAGLINE', get_bloginfo( 'description' ) );
+ // Wordpress PHP variables to render into JS at outset.
+
+  $array = array(
+    "SITE_URL" => get_site_url(),
+    "HOME_URL" => get_home_url(),
+    "SITE_TITLE" => get_bloginfo( 'name' ),
+    "SITE_TAGLINE" => get_bloginfo( 'description' ),
+    "active_id" => $post->ID,
+    "active_object" => $post->post_type,
+    "home_page" => get_option( 'page_on_front' ),
+    "useWheelNav" => false,
+    "uploads_path" =>  $url['baseurl'],
+    "slug" => $post->post_name,
+    "profile_template" => ''//hack
+    );
+
+
+
+  
+  wp_enqueue_script('config-script', get_template_directory_uri() . '/src/js/app/config/config.js');
+  wp_localize_script('config-script', 'configScriptData', $array);
+
+
 ?>
 
