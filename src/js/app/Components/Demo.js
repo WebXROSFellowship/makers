88c5d636eece2b366f3c81651e52bc20c99fb491
Ananya Jain <54437908+ananya2711@users.noreply.github.com>
2023-06-26 21:16:19 +0530

    progress with multiligualism

--- src/js/app/Components/Demo.js
+++ src/js/app/Components/Demo.js
@@ -1,18 +1,19 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect ,useRef } from "react";
 
 // Updated Inspector API data
-import {AppConfig} from "../config/appConfig";
+import { AppConfig } from "../config/appConfig";
 import assets from "./../../../../data/assets_demo.json";
 import data from "./../../../../data/dynamicContent_demo.json";
-import { DataContext } from "../utils";
+// import { DataContext } from "../utils";
 
 const Demo = () => {
-  const PAGE_SLUG="webxros-a-frame-demo";
+  const PAGE_SLUG = "webxros-a-frame-demo";
   const base_url = AppConfig.SITE_URL;
   const [loading, setLoading] = useState(true); // For asset loading
   const [scientistsData, setScientistsData] = useState([]);
   const [elementDetected, setElementDetected] = useState(false); // For inspector loaded
-  const { lang, setLang } = useContext(DataContext);
+  // const {lang, setLang } = useContext(DataContext);
+  const langRef = useRef("en");
   const [allLang, setAllLang] = useState([]);
   const [furnitureData, setFurnitureData] = useState([]);
   const [worldData, setWorldData] = useState([]);
@@ -20,9 +21,6 @@ const Demo = () => {
 
   useEffect(() => {
     getFromServer();
-  }, [lang]);
-
-  useEffect(() => {
     // Call the checkElement function initially
     checkElement();
     getLanguages();
@@ -38,7 +36,6 @@ const Demo = () => {
     return () => observer.disconnect();
   }, [elementDetected]);
 
-
   const getLanguages = async () => {
     const langFetchURL = `${base_url}/wp-json/wpml/v1/active_languages`;
     let langData = await fetch(langFetchURL);
@@ -47,7 +44,19 @@ const Demo = () => {
     console.log("ALL LANG", jsonLangData);
     setAllLang(jsonLangData);
   };
+  const handleButtonClick = (event) => {
 
+    console.log("Lang changed");
+    
+    console.log("I'm clicked");
+    console.log("EVENTT",event);
+
+    const buttonText =event.target.getAttribute("value");
+   
+    console.log( "buttonText",buttonText);
+    langRef.current = buttonText;
+
+  };
   const checkElement = () => {
     // Usage: Checks if the inspector has been opened for the first time
     const ele = document.querySelector(
@@ -63,31 +72,30 @@ const Demo = () => {
 
   const getFromServer = async () => {
     // console.log("Inside get from staging");
-    const url =`${base_url}/wp-json/wp/v2/pages?fields=id,type,title,content,slug,excerpt,languages,post_media,featured_media,screen_images,properties_3D,featured_video,cats,tags,type&filter[orderby]=ID&order=asc&per_page=100`;
+    const url = `${base_url}/wp-json/wp/v2/pages?fields=id,type,title,content,slug,excerpt,languages,post_media,featured_media,screen_images,properties_3D,featured_video,cats,tags,type&filter[orderby]=ID&order=asc&per_page=100`;
     console.log(url);
     await fetch(url)
       .then((response) => response.json())
       .then((result) => {
         console.log("!!!!!!!!!!!!!!!!!!!result", result);
         var pagecontents = [];
-        var furniture=[];
-        var world=[];
-        var navmesh=[];
+        var furniture = [];
+        var world = [];
+        var navmesh = [];
         result.map((item) => {
-          if (item.slug === PAGE_SLUG) {  
-           pagecontents = item.post_media.screen_image;
-            furniture=item.properties_3D.furniture;
-            console.log("furniture",furniture);
-            world=item.properties_3D.world_model;
-            console.log("world",world);
-            navmesh=item.properties_3D.nav_mesh;
-            console.log("navmesh",navmesh);
+          if (item.slug === PAGE_SLUG) {
+            pagecontents = item.post_media.screen_image;
+            furniture = item.properties_3D.furniture;
+            console.log("furniture", furniture);
+            world = item.properties_3D.world_model;
+            console.log("world", world);
+            navmesh = item.properties_3D.nav_mesh;
+            console.log("navmesh", navmesh);
           }
         });
         setFurnitureData(furniture);
         setWorldData(world);
         setMeshData(navmesh);
-        console.log("Page contents as in", pagecontents);
         setScientistsData(pagecontents);
         setLoading(false);
         AddClickEvent(pagecontents);
@@ -97,21 +105,36 @@ const Demo = () => {
       });
   };
 
-  function ShowDescription(Obj, data) {
+  function ShowDescription(Obj) {
     console.log("ShowDescription");
     console.log(Obj);
-
-    var children = Obj.querySelectorAll("a-troika-text");
-    // console.log("childeern", children);
-    if (children) {
-      var state = !children[0].getAttribute("visible");
-      children[0].setAttribute("visible", state);
-      // children[1].setAttribute("visible", state);
-      // children[2].setAttribute("visible", state);
-      // children[3].setAttribute("visible", state);
-      // children[4].setAttribute("visible", state);
-      // children[5].setAttribute("visible", state);
-    }
+    var children_lang = Obj.querySelectorAll("a-entity");
+    console.log("children_lang", children_lang);
+    children_lang?.forEach((item) => {
+      console.log("item", item);
+      console.log(langRef.current);
+      console.log(item.getAttribute("id"));
+      if (item.getAttribute("id") === langRef.current) {
+        var state = !item.getAttribute("visible");
+        console.log("!!!!!!!!!!!!item", langRef.current)
+        console.log("!!!!!!!!!!!!item", item)
+        item.setAttribute("visible", "true");
+      }
+      else {
+      item.setAttribute("visible", "false");
+      }
+    });
+    // var children = Obj.querySelectorAll("a-troika-text");
+    // // console.log("childeern", children);
+    // // if (children) {
+    //   // var state = !children[0].getAttribute("visible");
+    //   // children[0].setAttribute("visible", state);
+    //   // children[1].setAttribute("visible", state);
+    //   // children[2].setAttribute("visible", state);
+    //   // children[3].setAttribute("visible", state);
+    //   // children[4].setAttribute("visible", state);
+    //   // children[5].setAttribute("visible", state);
+    // // }
   }
 
   function customManipulation() {
@@ -208,6 +231,7 @@ const Demo = () => {
 
     if (!foundData && newData.id !== undefined && newData.class === undefined)
       updatedData.push(newData);
+
     if (newData.class !== undefined && !foundClassData) {
       console.log("New Class Data");
       var alteredClassData = updateClassData(newData);
@@ -242,13 +266,14 @@ const Demo = () => {
       .catch((error) => console.log("Error", error));
   };
 
-  function AddClickEvent(fdata) {
-    console.log("In add click event", fdata);
+  function AddClickEvent() {
+    console.log("In add click event");
     AFRAME.registerComponent("show-details-on-click", {
       init: function () {
         var el = this.el;
         el.addEventListener("click", function () {
-          ShowDescription(el, fdata);
+          ShowDescription(el);
+          console.log("Click detected",el);
           // UpdateProperties(data)
           // console.log("Click detected");
         });
@@ -256,19 +281,6 @@ const Demo = () => {
     });
   }
 
-  const handleButtonClick = (event) => {
-    console.log("Lang changed");
-    console.log("I'm clicked");
-    const buttonText = event.target.getAttribute("value");
-
-    if (buttonText === "English") {
-      setLang("");
-    } else if (buttonText === "Hindi") {
-      setLang("hi");
-    } else if (buttonText === "German") {
-      setLang("de");
-    }
-  };
 
   return (
     <>
@@ -315,6 +327,7 @@ const Demo = () => {
 
         {/* loaading assets in Aframe */}
         <a-assets>
+          {" "}
           {assets.map((asset) => {
             return (
               <a-asset-item
@@ -332,125 +345,125 @@ const Demo = () => {
           </div>
         ) : (
           <>
-            
-             <a-entity 
-                id={worldData.id}
-                gltf-model={base_url +"/wp-content/uploads/"+ worldData.src} 
-                key={worldData.id}
-                position="4.537 0 3.468"
-              >
-             </a-entity>
-
-              <a-entity
+            <a-entity
+              id={worldData.id}
+              gltf-model={base_url + "/wp-content/uploads/" + worldData.src}
+              key={worldData.id}
+              position="4.537 0 3.468"
+            ></a-entity>
+            <a-entity
               nav-mesh=""
               id={meshData.id}
               gltf-model={base_url + "/wp-content/uploads/" + meshData.src}
               key={meshData.id}
               visible="false"
               position="4.762 0 3.739"
-              ></a-entity>
-
-              {
-                furnitureData?.map((furniture) => {
-                var Obj_id = furniture.id;
-                var Data_from_Inspector = data.find((obj) => obj.id == Obj_id);
-                  if (!Data_from_Inspector) {
-                    Data_from_Inspector={
-                      position: "0 0 0",
-                    }
-                  }
-                  return (
-                    <a-entity
-                      id={furniture.id}
-                      gltf-model={base_url + furniture.full_path}
-                      key={furniture.id}
-                      {...Data_from_Inspector}
-                    ></a-entity>)
-                })
+            ></a-entity>
+            {furnitureData?.map((furniture) => {
+              var Obj_id = furniture.id;
+              var Data_from_Inspector = data.find((obj) => obj.id == Obj_id);
+              if (!Data_from_Inspector) {
+                Data_from_Inspector = {
+                  position: "0 0 0",
+                };
               }
+              return (
+                <a-entity
+                  id={furniture.id}
+                  gltf-model={base_url + furniture.full_path}
+                  key={furniture.id}
+                  {...Data_from_Inspector}
+                ></a-entity>
+              );
+            })}
             {scientistsData?.map((scientist) => {
-             
-                var Obj_id = scientist.id;
-                console.log(Obj_id);
-                console.log(data);
-                var Data_from_Inspector = data.find((obj) => obj.id == Obj_id);
-                var desc_format = data.find((obj) => obj.class == "desc_wrapper");
-                var cap_format = data.find(
-                  (obj) => obj.class == "caption_wrapper"
-                );
-                var name_format = data.find((obj) => obj.class == "name_wrapper");
-                var img_format = data.find((obj) => obj.class == "image_wrapper");
-                // console.log("CHECK",scientist);
-                  if (!Data_from_Inspector) {
-                    Data_from_Inspector={
-                      position: "0 0 0",
-                    }
-                  }
-                  // console.log("position", Data_from_Inspector.position);
+              var Obj_id = scientist.id;
+              var Data_from_Inspector = data.find((obj) => obj.id == Obj_id);
+              var desc_format = data.find((obj) => obj.class == "desc_wrapper");
+              var cap_format = data.find(
+                (obj) => obj.class == "caption_wrapper"
+              );
+              var name_format = data.find((obj) => obj.class == "name_wrapper");
+              var img_format = data.find((obj) => obj.class == "image_wrapper");
+              // console.log("CHECK",scientist);
+              if (!Data_from_Inspector) {
+                Data_from_Inspector = {
+                  position: "0 0 0",
+                };
+              }
+              delete Data_from_Inspector["show-details-on-click"];
+              // console.log("position", Data_from_Inspector.position);
+              return (
+                <a-entity
+                  id={scientist.id}
+                  type="wrapper"
+                  key={scientist.id}
+                  {...Data_from_Inspector}
+                  show-details-on-click=""
+                >
+                  <a-image
+                    src={base_url + scientist.full_path}
+                    {...img_format}
+                    type="wrapper"
+                    class="image_wrapper"
+                  ></a-image>
+                  {allLang?.map((lang) => {
                   return (
-                    <a-entity
-                      id={scientist.id}
+                  <a-entity key={lang.code} id={lang.code} visible="false">
+                    <a-troika-text
+                      class="desc_wrapper"
                       type="wrapper"
-                      key={scientist.id}
-                      {...Data_from_Inspector}
-                      show-details-on-click=""
-                    >
-                      <a-image
-                        src={base_url + scientist.full_path}
-                        {...img_format}
-                        type="wrapper"
-                        class="image_wrapper"
-                      ></a-image>
-                      <a-troika-text
-                        class="desc_wrapper"
-                        type="wrapper"
-                        value={scientist.alt}
-                        font= {base_url + "/wp-content/uploads/2023/06/NotoSans-Medium.ttf"}
-                        visible="false"
-                        {...desc_format}
-                      ></a-troika-text>
-                      <a-troika-text
-                        class="caption_wrapper"
-                        type="wrapper"
-                        value={scientist.caption}
-                        font= {base_url + "/wp-content/uploads/2023/06/NotoSans-Medium.ttf"}
-                       
-                        {...cap_format}
-                      ></a-troika-text>
-                      <a-troika-text
-                        class="name_wrapper"
-                        type="wrapper"
-                        value={scientist.title}
-                        font= {base_url + "/wp-content/uploads/2023/06/NotoSans-Medium.ttf"}
-                      
-                        {...name_format}
-                      ></a-troika-text>
-                      {
-                        Object.keys(scientist.trans).map((key)=>{
-                          // console.log("key",scientist.trans[key]);
-                          var classname="btn-wrapper-"+key;
-                          var insData=data.find((obj) => obj.class == classname);
-                          return (
-                            <a-troika-text
-                              class={classname}
-                              type="wrapper"
-                              visible="true"
-                              key={classname}
-                              value={key}
-                              code={key}
-                              onClick={(e)=>{
-                                console.log("Lang changed");
-                                let langCode = e.target.getAttribute("value");
-                                langCode == "en" ? "" : langCode;
-                                console.log("Setting lang as, ",langCode);
-                                setLang(langCode);
-                              }}
-                              {...insData}
-                            ></a-troika-text>
-                          );
-                        })
+                      value={scientist.trans[lang.code].alt}
+                      font={
+                        base_url +
+                        "/wp-content/uploads/2023/06/NotoSans-Medium.ttf"
                       }
-                      {/* {allLang?.map((lang) => {
+                      visible="true"
+                      {...desc_format}
+                    ></a-troika-text>
+                    <a-troika-text
+                      class="caption_wrapper"
+                      type="wrapper"
+                      value={scientist.trans[lang.code].caption}
+                      font={
+                        base_url +
+                        "/wp-content/uploads/2023/06/NotoSans-Medium.ttf"
+                      }
+                      visible="true"
+                      {...cap_format}
+                    ></a-troika-text>
+                    <a-troika-text
+                      class="name_wrapper"
+                      type="wrapper"
+                      value={scientist.trans[lang.code].title}
+                      font={
+                        base_url +
+                        "/wp-content/uploads/2023/06/NotoSans-Medium.ttf"
+                      }
+                      visible="true"
+                      {...name_format}
+                    ></a-troika-text>
+                  </a-entity>
+                  );
+                  })}
+                  {Object.keys(scientist.trans).map((key) => {
+                    // console.log("key",scientist.trans[key]);
+                    var classname = "btn-wrapper-" + key;
+                    var insData = data.find((obj) => obj.class == classname);
+                    return (
+                      <a-troika-text
+                        class={classname}
+                        type="wrapper"
+                        visible="true"
+                        key={classname}
+                        value={key}
+                        code={key}
+                        onClick={handleButtonClick}
+                        {...insData}
+                      ></a-troika-text>
+                    );
+                  })}
+                  {/* {allLang?.map((lang) => {
                       var classname="btn-wrapper-"+lang.code;
                       var insData=data.find((obj) => obj.class == classname);
                       return (
@@ -465,12 +478,10 @@ const Demo = () => {
                           {...insData}
                         ></a-troika-text>
                       );
-                    })} */}
-                    </a-entity>
-                  );
-                
-              }
-            )}
+                    })} */}{" "}
+                </a-entity>
+              );
+            })}{" "}
           </>
         )}
 
